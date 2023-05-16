9a0fbc3b92654a33a577759ae396a860aef8d8af
DearXuan <1320699768@qq.com>
2023-05-17 00:48:01 +0800

    更新配置信息和文本

--- src/main/java/com/dearxuan/easyhopper/Config/ModConfig.java
+++ src/main/java/com/dearxuan/easyhopper/Config/ModConfig.java
@@ -3,7 +3,6 @@ package com.dearxuan.easyhopper.Config;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.Excluded;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
@@ -15,7 +14,7 @@ public class ModConfig implements ConfigData {
     @Excluded
     public static ModConfig INSTANCE;
 
-    @Comment("控制漏斗每多少个tick输送一次物品")
+    @Comment("控制漏斗输送物品冷却时间")
     public int TRANSFER_COOLDOWN = 8;
 
     @Comment("控制漏斗每次输入多少个物品")

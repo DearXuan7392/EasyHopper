5b58627f36af56be7f57e6b5638a403cc985db9d
DearXuan <1320699768@qq.com>
2023-05-21 22:49:49 +0800

    Fix some language problem

--- src/main/java/com/dearxuan/easyhopper/Config/ModConfig.java
+++ src/main/java/com/dearxuan/easyhopper/Config/ModConfig.java
@@ -5,25 +5,21 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry.Gui.Excluded;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(
         name = "easyhopper"
 )
 public class ModConfig implements ConfigData {
+
     @Excluded
     public static ModConfig INSTANCE;
 
-    @Comment("控制漏斗输送物品冷却时间")
     public int TRANSFER_COOLDOWN = 8;
 
-    @Comment("控制漏斗每次输入多少个物品")
     public int TRANSFER_INPUT_COUNT = 1;
 
-    @Comment("控制漏斗每次输出多少个物品")
     public int TRANSFER_OUTPUT_COUNT = 1;
 
-    @Comment("若启用,会占用漏斗的最后一格作为分类格,这个漏斗只会允许与最后一格相同的物品通过")
     public boolean CLASSIFICATION_HOPPER = false;
 
     public ModConfig(){
--- src/main/java/com/dearxuan/easyhopper/Config/ModMenu.java
+++ src/main/java/com/dearxuan/easyhopper/Config/ModMenu.java
@@ -1,23 +1,113 @@
 package com.dearxuan.easyhopper.Config;
 
+import com.dearxuan.easyhopper.EasyHopper;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.api.*;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
+
+import java.lang.reflect.Field;
 
 @Environment(EnvType.CLIENT)
 public class ModMenu implements ModMenuApi {
 
+    private static final String HEAD = "easyhopper";
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory(){
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> {
+            try{
+                ConfigBuilder builder = ConfigBuilder
+                        .create()
+                        .setParentScreen(parent)
+                        .setTitle(Text.translatable(HEAD + ".title"));
+                builder.setSavingRunnable(()->{
+                    AutoConfig.getConfigHolder(ModConfig.class).save();
+                });
+                ConfigCategory general = builder
+                        .getOrCreateCategory(Text.translatable(HEAD + ".title"));
+                ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+                general
+                        .addEntry(BuildConfig(entryBuilder, "TRANSFER_COOLDOWN", 8))
+                        .addEntry(BuildConfig(entryBuilder, "TRANSFER_INPUT_COUNT", 1))
+                        .addEntry(BuildConfig(entryBuilder, "TRANSFER_OUTPUT_COUNT", 1))
+                        .addEntry(BuildConfig(entryBuilder, "CLASSIFICATION_HOPPER", false));
+                Screen screen = builder.build();
+                return screen;
+            }catch (Exception e){
+                EasyHopper.LOGGER.error(e.getMessage());
+                return null;
+            }
+        };
+    }
+
+    private static AbstractConfigListEntry BuildConfig(ConfigEntryBuilder entryBuilder, String name, int defaultValue){
+        try{
+            String Text_Name = HEAD + "." + name;
+            String Text_Tooltip = Text_Name + ".tooltip";
+            Field field = ModConfig.class.getDeclaredField(name);
+            return entryBuilder
+                    .startIntField(Text.translatable(Text_Name), field.getInt(ModConfig.INSTANCE))
+                    .setDefaultValue(defaultValue)
+                    .setTooltip(Text.translatable(Text_Tooltip))
+                    .setSaveConsumer(value -> {
+                        try {
+                            field.setInt(ModConfig.INSTANCE, value);
+                        } catch (IllegalAccessException e) {
+                            EasyHopper.LOGGER.error(e.getMessage());
+                        }
+                    })
+                    .build();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static AbstractConfigListEntry BuildConfig(ConfigEntryBuilder entryBuilder, String name, double defaultValue){
+        try{
+            String Text_Name = HEAD + "." + name;
+            String Text_Tooltip = Text_Name + ".tooltip";
+            Field field = ModConfig.class.getDeclaredField(name);
+            return entryBuilder
+                    .startDoubleField(Text.translatable(Text_Name), field.getDouble(ModConfig.INSTANCE))
+                    .setDefaultValue(defaultValue)
+                    .setTooltip(Text.translatable(Text_Tooltip))
+                    .setSaveConsumer(value -> {
+                        try {
+                            field.setDouble(ModConfig.INSTANCE, value);
+                        } catch (IllegalAccessException e) {
+                            EasyHopper.LOGGER.error(e.getMessage());
+                        }
+                    })
+                    .build();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static AbstractConfigListEntry BuildConfig(ConfigEntryBuilder entryBuilder, String name, boolean defaultValue){
+        try{
+            String Text_Name = HEAD + "." + name;
+            String Text_Tooltip = Text_Name + ".tooltip";
+            Field field = ModConfig.class.getDeclaredField(name);
+            return entryBuilder
+                    .startBooleanToggle(Text.translatable(Text_Name), field.getBoolean(ModConfig.INSTANCE))
+                    .setDefaultValue(defaultValue)
+                    .setTooltip(Text.translatable(Text_Tooltip))
+                    .setSaveConsumer(value -> {
+                        try {
+                            field.setBoolean(ModConfig.INSTANCE, value);
+                        } catch (IllegalAccessException e) {
+                            EasyHopper.LOGGER.error(e.getMessage());
+                        }
+                    })
+                    .build();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
